9d624509bd6681e4128ab19f2dd48bb549150d84
Harmony Trevena <harmonytrevena@gmail.com>
2020-09-22 16:53:21 -0400

    Added CSS and connected task form to db.

--- models/taskModel.js
+++ models/taskModel.js
@@ -2,15 +2,25 @@
 const db = require('./conn');
 
 class TaskList {
-    constructor(task_title, task_details, category, due_date) {
+    constructor(task_title, task_details, category, due_date, task_status) {
         this.task_title = task_title;
         this.task_details = task_details;
         this.category = category;
         this.due_date = due_date;
+        this.task_status = task_status;
     }
     static async getAll() {
         try {
             const response = await db.any(`SELECT * FROM task ORDER BY task.id;`);
+            console.log(response);
+            return response;
+        } catch (error) {
+            return error.message;
+        }
+    }
+    static async addTask(task_title, task_details, category, due_date, user_id, task_status) {
+        try {
+            const response = await db.result(`INSERT INTO task (task_title, task_details, category, due_date, user_id, task_status) VALUES ($1, $2, $3, $4, $5, $6);`, [task_title, task_details, category, due_date, user_id, task_status]);
             console.log(response);
             return response;
         } catch (error) {
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,18 +1,30 @@
 'use strict'
 
 const express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    taskModel = require('../models/taskModel')
 
 router.get("/", async (req, res) => {
+    const taskData = await taskModel.getAll();
+
+    console.log(taskData)
     res.render("template", {
         locals: {
             title: "Tasks",
+            data: taskData,
             is_logged_in: req.session.is_logged_in
         },
         partials: {
             partial: "partial-task"
         }
     });
+});
+
+router.post('/', async function (req, res) {
+    const { task_title, task_details, category, due_date, user_id, task_status } = req.body;
+    console.log(req.body);
+    await taskModel.addTask(task_title, task_details, category, due_date, user_id, task_status);
+    res.redirect('/tasks');
 });
 
 module.exports = router;
